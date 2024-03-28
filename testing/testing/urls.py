e973af965959faabe02e39f353ebb92e7b6adcb8
Polantvic <polantvic@mail.ru>
2024-03-28 13:15:10 +0200

    adding css +settings

--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path('', include('questions.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
